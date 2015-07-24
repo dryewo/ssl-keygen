5bb6728a44c1a960ab74877b87df19e3d70b6dd1
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-07-24 15:28:25 +0200

    Clean up

--- gen-cert.sh
+++ gen-cert.sh
@@ -13,14 +13,8 @@ IFS=$'\t\n'
 # Generates cert.key and cert.crt files for a specified domain name
 
 HOSTNAME=$1
-#FILENAME=${2:-$CN}
 
 echo Generating SSL certificate for "$HOSTNAME"
-
-#openssl genrsa -des3 -passout pass:x -out ${FILENAME}.pass.key 2048
-#openssl rsa -passin pass:x -in ${FILENAME}.pass.key -out ${FILENAME}.key
-#openssl req -new -key ${FILENAME}.key -out ${FILENAME}.csr -subj "/CN=${CN}"
-#openssl x509 -req -days 365 -in ${FILENAME}.csr -signkey ${FILENAME}.key -out ${FILENAME}.crt
 
 openssl genrsa -out cert.key 4096
 # Use piping to avoid *.csr file creation
